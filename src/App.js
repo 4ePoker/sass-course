2939ce36d24e8e43c3afb57953e017abc3da7106
PkerOk44 <107943031+4ePoker@users.noreply.github.com>
2023-05-25 22:08:47 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './stylesheets/app.scss'
+
+function App() {
+    return (
+        <div>
+            <div>
+
+            </div>
+            <div className="card">
+                <h2>Card</h2>
+                <h1 className="card-title">This is the card title</h1>
+                <p className="card-body">Lorem ipsum dolor sit amet, consectetur
+                    adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Suspendisse faucibus interdum posuere
+                    lorem ipsum dolor sit amet.
+                    <a href='#'>
+                        Quam adipiscing vitae proin sagittis.
+                    </a>
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default App;
